b8f3503667392943a3fc82ac194934fe47695b7c
Wang Jiaxiang <wangjiaxiangyx@163.com>
2019-05-21 00:05:03 +0800

    Python-100-Days(01、02)
    Learn about Pytho-100-Days
    day-01 & day02

---
+++ PythonCoding/python_basic_01/01-初识Python.py
@@ -0,0 +1,16 @@
+# ****************************
+# 初识Python01  -- 01 学习使用turtle在屏幕上绘制图形
+# ****************************
+
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('red')
+turtle.forward(100)
+turtle.right(60)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.mainloop()
