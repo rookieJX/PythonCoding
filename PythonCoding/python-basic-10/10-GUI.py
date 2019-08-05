c1864f4ab38123d83d3bd890143a66793d42036a
Wang Jiaxiang <wangjiaxiangyx@163.com>
2019-08-06 00:10:05 +0800

    Python-100-Days(10)
    大球吃小球游戏

--- PythonCoding/python-basic-10/10-GUI.py
+++ PythonCoding/python-basic-10/10-GUI.py
@@ -64,5 +64,187 @@ def main():
     # 开启主时间循环
     tkinter.mainloop()
 
+
+
+'''
+使用Pygame进行游戏开发
+Pygame是一个开源的Python模块，专门用于多媒体应用（如电子游戏）的开发，
+其中包含对图像、声音、视频、事件、碰撞等的支持。
+Pygame建立在SDL的基础上。SDL是一套跨平台的多媒体开发库，用C语言实现。
+'''
+
+
+import pygame
+
+# 制作游戏窗口
+def game_screen_main():
+    # 初始化导入的pygame中的模块
+    pygame.init()
+    # 初始化用于显示的窗口并设置窗口尺寸
+    screen = pygame.display.set_mode((800,600))
+    # 设置当前窗口的标题
+    pygame.display.set_caption('大球吃小球')
+    running = True
+    #开启一个事件循环处理发生的事件
+    while running:
+        # 从消息队列中获取事件并对事件进行处理
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+
+# 在窗口中绘图
+def game_drwan_main():
+    # 初始化
+    pygame.init()
+    # 尺寸
+    screen = pygame.display.set_mode((800,600))
+    # 设置标题
+    pygame.display.set_caption('大球吃小球')
+    screen.fill((242,242,242))
+    pygame.draw.circle(screen,(255,0,145),(100,100),30,0)
+    pygame.display.flip()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+# 加载图像
+def game_image_main():
+    pygame.init()
+    screen = pygame.display.set_mode((800,600))
+    pygame.display.set_caption('----')
+    screen.fill((255,120,230))
+    ball_iamge = pygame.image.load('./res/ball.png')
+    screen.blit(ball_iamge,(50,50))
+    pygame.display.flip()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+# 加载动画
+def game_animate_main():
+    pygame.init()
+    screen = pygame.display.set_mode((800,600))
+    pygame.display.set_caption('---')
+    x,y = 50,50
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((234,123,120))
+        pygame.draw.circle(screen,(255,0,0),(x,y),30,0)
+        pygame.display.flip()
+        pygame.time.delay(3)
+        x,y = x+5,y+5
+
+# 碰撞检测
+from enum import Enum,unique
+from math import sqrt
+from random import randint
+
+@unique
+class Color(Enum):
+    '''颜色'''
+
+    RED = (255,0,0)
+    GREEN = (0,255,0)
+    BLUE = (0,0,255)
+    BLACK = (0,0,0)
+    WHITE = (255,255,255)
+    GRAY = (242,242,242)
+
+    @staticmethod
+    def random_color():
+        '''获得随机颜色'''
+        r = randint(0,255)
+        g = randint(0,255)
+        b = randint(0,255)
+        return (r,g,b)
+
+
+class Ball(object):
+    '''球'''
+
+    def __init__(self,x,y,radius,sx,sy,color=Color.RED):
+        '''初始化方法'''
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.sx = sx
+        self.sy = sy
+        self.color = color
+        self.alive = True
+
+    def move(self,screen):
+        '''移动'''
+        self.x += self.sx
+        self.y += self.sy
+
+        if self.x - self.radius <= 0 or \
+            self.x + self.radius >= screen.get_width():
+            self.sx = -self.sx
+
+        if self.y - self.radius <= 0 or \
+            self.y + self.radius >= screen.get_height():
+            self.sy = - self.sy
+
+    def eat(self,other):
+        '''吃其他球'''
+
+        if self.alive and other.alive and self != other:
+            dx,dy = self.x - other.x,self.y - other.y
+            distance = sqrt(dx ** 2 + dy **2)
+            if distance < self.radius + other.radius \
+                and self.radius > other.radius:
+                other.alive = False
+                self.radius = self.radius + int(other.radius * 0.146)
+
+    def draw(self,screen):
+        '''在窗口上绘制球'''
+        pygame.draw.circle(screen,self.color,(self.x,self.y),self.radius,0)
+
+
+def game_event_main():
+
+    balls = []
+    pygame.init()
+    screen = pygame.display.set_mode((1600, 1200))
+    pygame.display.set_caption('大球游戏')
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                x,y = event.pos
+                radius = randint(1,10)
+                sx,sy = randint(-10,10),randint(-10,10)
+                color = Color.random_color()
+                ball = Ball(x,y,radius,sx,sy,color)
+                balls.append(ball)
+        screen.fill((255,255,255))
+        for ball in balls:
+            if ball.alive:
+                ball.draw(screen)
+            else:
+                balls.remove(ball)
+
+        pygame.display.flip()
+        pygame.time.delay(1)
+        for ball in balls:
+            ball.move(screen)
+            for other in balls:
+                ball.eat(other)
+
 if __name__ == '__main__':
-    main()
+    # main()
+    # game_screen_main()
+    # game_drwan_main()
+    # game_image_main()
+    # game_animate_main()
+    game_event_main()
